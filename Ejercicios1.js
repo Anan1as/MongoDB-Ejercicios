f8a298d2e80e8290d01cad9c786f0f7cf84b27a3
Ananias <amychelleoropezaortiz@gmail.com>
2024-03-15 20:53:18 -0500

    Agregado el playground completo.

---
+++ Ejercicios1.js
@@ -0,0 +1,396 @@
+db.createCollection('ussers')
+
+db.ussers.insertMany([
+    {
+        nombres: "John",
+        apellidos: "Doe",
+        correo: "john.doe@example.com",
+        ciudad: "New York",
+        pais: "Estados Unidos",
+        salario: 60000,
+        edad: 35,
+        altura: 180,
+        peso: 180,
+    },
+    {
+        nombres: "Alice",
+        apellidos: "Smith",
+        correo: "alice.smith@example.com",
+        ciudad: "London",
+        pais: "Reino Unido",
+        salario: 55000,
+        edad: 30,
+        altura: 165,
+        peso: 150,
+    },
+    {
+        nombres: "Juan",
+        apellidos: "Pérez",
+        correo: "juan.perez@example.com",
+        ciudad: "Madrid",
+        pais: "España",
+        salario: 48000,
+        edad: 40,
+        altura: 175,
+        peso: 160,
+    },
+    {
+        nombres: "Maria",
+        apellidos: "García",
+        correo: "maria.garcia@example.com",
+        ciudad: "Paris",
+        pais: "Francia",
+        salario: 52000,
+        edad: 32,
+        altura: 170,
+        peso: 140,
+    },
+    {
+        nombres: "Marco",
+        apellidos: "Rossi",
+        correo: "marco.rossi@example.com",
+        ciudad: "Rome",
+        pais: "Italia",
+        salario: 49000,
+        edad: 37,
+        altura: 178,
+        peso: 165,
+    },
+    {
+        nombres: "Luis",
+        apellidos: "Fernández",
+        correo: "luis.fernandez@example.com",
+        ciudad: "Barcelona",
+        pais: "España",
+        salario: 51000,
+        edad: 28,
+        altura: 172,
+        peso: 155,
+    },
+    {
+        nombres: "Ana",
+        apellidos: "Silva",
+        correo: "ana.silva@example.com",
+        ciudad: "Rio de Janeiro",
+        pais: "Brasil",
+        salario: 47000,
+        edad: 33,
+        altura: 168,
+        peso: 145,
+    },
+    {
+        nombres: "Diego",
+        apellidos: "Gonzalez",
+        correo: "diego.gonzalez@example.com",
+        ciudad: "Buenos Aires",
+        pais: "Argentina",
+        salario: 48000,
+        edad: 31,
+        altura: 175,
+        peso: 160,
+    },
+    {
+        nombres: "Emily",
+        apellidos: "Johnson",
+        correo: "emily.johnson@example.com",
+        ciudad: "Sydney",
+        pais: "Australia",
+        salario: 60000,
+        edad: 29,
+        altura: 167,
+        peso: 140,
+    },
+    {
+        nombres: "Michael",
+        apellidos: "Brown",
+        correo: "michael.brown@example.com",
+        ciudad: "Toronto",
+        pais: "Canadá",
+        salario: 55000,
+        edad: 35,
+        altura: 180,
+        peso: 175,
+    },
+    {
+        nombres: "Sophia",
+        apellidos: "Lee",
+        correo: "sophia.lee@example.com",
+        ciudad: "Seoul",
+        pais: "Corea del Sur",
+        salario: 52000,
+        edad: 34,
+        altura: 168,
+        peso: 150,
+    },
+    {
+        nombres: "Mohammed",
+        apellidos: "Khan",
+        correo: "mohammed.khan@example.com",
+        ciudad: "Mumbai",
+        pais: "India",
+        salario: 50000,
+        edad: 32,
+        altura: 175,
+        peso: 160,
+    },
+    {
+        nombres: "Olivia",
+        apellidos: "Nguyen",
+        correo: "olivia.nguyen@example.com",
+        ciudad: "Ho Chi Minh City",
+        pais: "Vietnam",
+        salario: 47000,
+        edad: 29,
+        altura: 163,
+        peso: 135,
+    },
+    {
+        nombres: "Mia",
+        apellidos: "Garcia",
+        correo: "mia.garcia@example.com",
+        ciudad: "Los Angeles",
+        pais: "Estados Unidos",
+        salario: 60000,
+        edad: 31,
+        altura: 170,
+        peso: 145,
+    },
+    {
+        nombres: "Ethan",
+        apellidos: "Martinez",
+        correo: "ethan.martinez@example.com",
+        ciudad: "Dallas",
+        pais: "Estados Unidos",
+        salario: 57000,
+        edad: 33,
+        altura: 175,
+        peso: 160,
+    },
+    {
+        nombres: "Amelia",
+        apellidos: "Wilson",
+        correo: "amelia.wilson@example.com",
+        ciudad: "London",
+        pais: "Reino Unido",
+        salario: 54000,
+        edad: 30,
+        altura: 168,
+        peso: 155,
+    },
+    {
+        nombres: "William",
+        apellidos: "Taylor",
+        correo: "william.taylor@example.com",
+        ciudad: "Melbourne",
+        pais: "Australia",
+        salario: 58000,
+        edad: 34,
+        altura: 180,
+        peso: 170,
+    },
+    {
+        nombres: "Charlotte",
+        apellidos: "Anderson",
+        correo: "charlotte.anderson@example.com",
+        ciudad: "Montreal",
+        pais: "Canadá",
+        salario: 55000,
+        edad: 32,
+        altura: 170,
+        peso: 160,
+    },
+    {
+        nombres: "Alexander",
+        apellidos: "Martinez",
+        correo: "alexander.martinez@example.com",
+        ciudad: "Mexico City",
+        pais: "México",
+        salario: 48000,
+        edad: 31,
+        altura: 175,
+        peso: 165,
+    },
+    {
+        nombres: "Ava",
+        apellidos: "Brown",
+        correo: "ava.brown@example.com",
+        ciudad: "Toronto",
+        pais: "Canadá",
+        salario: 56000,
+        edad: 35,
+        altura: 173,
+        peso: 155,
+    },
+    {
+        nombres: "Lucas",
+        apellidos: "Rodriguez",
+        correo: "lucas.rodriguez@example.com",
+        ciudad: "Buenos Aires",
+        pais: "Argentina",
+        salario: 49000,
+        edad: 29,
+        altura: 178,
+        peso: 170,
+    },
+    {
+        nombres: "Emma",
+        apellidos: "Lee",
+        correo: "emma.lee@example.com",
+        ciudad: "Sydney",
+        pais: "Australia",
+        salario: 59000,
+        edad: 33,
+        altura: 168,
+        peso: 150,
+    },
+    {
+        nombres: "Daniel",
+        apellidos: "Gonzalez",
+        correo: "daniel.gonzalez@example.com",
+        ciudad: "Santiago",
+        pais: "Chile",
+        salario: 48000,
+        edad: 30,
+        altura: 175,
+        peso: 160,
+    },
+    {
+        nombres: "Sophia",
+        apellidos: "Moreno",
+        correo: "sophia.moreno@example.com",
+        ciudad: "Bogotá",
+        pais: "Colombia",
+        salario: 50000,
+        edad: 32,
+        altura: 170,
+        peso: 145,
+    },
+    {
+        nombres: "Benjamin",
+        apellidos: "Lee",
+        correo: "benjamin.lee@example.com",
+        ciudad: "Seoul",
+        pais: "Corea del Sur",
+        salario: 51000,
+        edad: 28,
+        altura: 175,
+        peso: 155,
+    },
+    {
+        nombres: "Sofia",
+        apellidos: "Rodriguez",
+        correo: "sofia.rodriguez@example.com",
+        ciudad: "Buenos Aires",
+        pais: "Argentina",
+        salario: 47000,
+        edad: 31,
+        altura: 163,
+        peso: 140,
+    },
+    {
+        nombres: "Oliver",
+        apellidos: "Kim",
+        correo: "oliver.kim@example.com",
+        ciudad: "Seoul",
+        pais: "Corea del Sur",
+        salario: 52000,
+        edad: 33,
+        altura: 178,
+        peso: 160,
+    },
+    {
+        nombres: "Emily",
+        apellidos: "Gomez",
+        correo: "emily.gomez@example.com",
+        ciudad: "São Paulo",
+        pais: "Brasil",
+        salario: 48000,
+        edad: 29,
+        altura: 170,
+        peso: 150,
+    },
+    {
+        nombres: "Noah",
+        apellidos: "Nguyen",
+        correo: "noah.nguyen@example.com",
+        ciudad: "Sydney",
+        pais: "Australia",
+        salario: 57000,
+        edad: 32,
+        altura: 175,
+        peso: 165,
+    },
+    {
+        nombres: "Isabella",
+        apellidos: "Martinez",
+        correo: "isabella.martinez@example.com",
+        ciudad: "Montréal",
+        pais: "Canadá",
+        salario: 55000,
+        edad: 30,
+        altura: 168,
+        peso: 150,
+    }
+]);
+
+
+//-------------------Ejercicios #1
+//1. Obtener todos los usuarios que sean mayores de 18 años.
+db.ussers.find({edad: {$gt: 18}});
+
+//2. Obtener todos los usuarios que sean de Londres o de París.
+db.ussers.find({$or: [{ciudad: "London"}, {ciudad: "Paris"}]});
+
+//3. Obtener a todos los usuarios que ganen más de $2000 al mes y tengan menos de 30 años.
+db.ussers.find({$and: [{salario: {$gt: 2000}}, {edad: {$lt: 30}}]});
+
+//4. Obtener a todos los usuarios que sean de España y ganen más de $3000 al mes.
+db.ussers.find({$and: [{pais: "España"}, {salario: {$gt: 3000}}]});
+
+//5. Obtener todos los usuarios que tengan entre 25 y 35 años.
+db.ussers.find({edad: {$gt: 25, $lt: 35}});
+
+//6. Obtener a todos los usuarios que no sean de Estados Unidos.
+db.ussers.find({pais: {$ne: "Estados Unidos"}});
+
+//7. Obtener a todos los usuarios que sean de Londres y que ganen más de $2500 o que tengan más de 30 años.
+db.ussers.find({$and: [{ciudad: "London"}, {$or: [{salario: {$gt: 2500}}, {edad: {$gt: 30}}]}]});
+
+//8. Obtener a todos los usuarios que sean de Australia y tengan un peso mayor a 140 libras.
+db.ussers.find({$and: [{pais: "Australia"}, {peso: {$gt: 140}}]});
+
+//9. Obtener a todos los usuarios que no sean de Londres ni de París.
+db.ussers.find({$and: [{ciudad: {$ne: "London"}}, {ciudad: {$ne: "Paris"}}]});
+
+//10. Obtener a todos los usuarios que ganen menos de $2000 al mes o que tengan más de 40 años.
+db.ussers.find({$or: [{salario: {$lt: 2000}}, {edad: {$gt: 40}}]});
+
+//11. Obtener a todos los usuarios que sean de Canadá y ganen más de $4000 al mes o que tengan una altura mayor a 180 cm.
+db.ussers.find({$and: [{pais: "Canadá"}, {$or: [{salario: {$gt: 4000}}, {altura: {$gt: 180}}]}]});
+
+//12. Obtener todos los usuarios que sean de Italia y tengan entre 20 y 30 años.
+db.ussers.find({$and: [{pais: "Italia"}, {edad: {$gt: 20, $lt: 30}}]});
+
+//13. Obtener todos los usuarios que no tengan un correo electrónico registrado.
+db.ussers.find({correo: {$exists: false}});
+
+//14. Obtener todos los usuarios que sean de Francia y que su salario esté entre $3000 y $5000 al mes.
+db.ussers.find({$and: [{pais: "Francia"}, {salario: {$gt: 3000, $lt: 5000}}]});
+
+//15. Obtener todos los usuarios que sean de Brasil y que tengan un peso menor a 120 libras o más de 140 libras.
+db.ussers.find({$and: [{pais: "Brasil"}, {$or: [{peso: {$lt: 120}}, {peso: {$gt: 140}}]}]});
+
+//16. Obtener todos los usuarios que sean de Argentina o de Chile y que tengan una edad menor a 25 años.
+db.ussers.find({$and: [{$or: [{pais: "Argentina"}, {pais: "Chile"}]}, {edad: {$lt: 25}}]});
+
+//17. Obtener a todos los usuarios que no sean de España ni de México y que ganen menos de $3000 al mes.
+db.ussers.find({$and: [{pais: {$ne: "España"}}, {pais: {$ne: "México"}}, {salario: {$lt: 3000}}]});
+
+//18. Obtener todos los usuarios que sean de Alemania y que tengan un salario menor a $4000 o una edad mayor a 35 años.
+db.ussers.find({$and: [{pais: "Alemania"}, {$or: [{salario: {$lt: 4000}}, {edad: {$gt: 35}}]}]});
+
+//19. Obtener todos los usuarios que no sean de Colombia y que su altura sea menor a 170 cm.
+db.ussers.find({$and: [{pais: {$ne: "Colombia"}}, {altura: {$lt: 170}}]});
+
+//20. Obtener todos los usuarios que sean de India y que no tengan un salario registrado.
+db.ussers.find({$and: [{pais: "India"}, {salario: {$exists: false}}]});
